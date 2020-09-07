9c4ed81a02e8f648aa780784e765374a172191aa
jundic <jundi_chen@sfu.ca>
2020-09-07 07:36:35 -0700

    a

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   //   app.use(express.static(path.join(__dirname, "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve("client", "build", "index.html"));
   });
 }
 
