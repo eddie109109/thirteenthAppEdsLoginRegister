28e6d3f01b7a4a166b6e5d6efc61063d61537318
jundic <jundi_chen@sfu.ca>
2020-09-07 07:19:53 -0700

    fix route

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const route = require("./route");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, "build"))); // connect to the static file through express
+
+const routes = require("./routes");
+app.use(express.static(path.join(__dirname, "build"))); // connect to the static file through express
 
 //direct the user to the built version of react front page, it only works once it is built(yarn built)
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+
+app.use("/", route);
 
 // server static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  //   app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
