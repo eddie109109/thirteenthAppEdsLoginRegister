6637fb4aaf47e820c8d25ff99e24570f6addc71c
jundic <jundi_chen@sfu.ca>
2020-09-07 05:24:25 -0700

    first commit

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "build"))); // connect to the static file through express
 
 app.get("/", (req, res) => {
-  res.send(200);
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//direct the user to the built version of react front page, it only works once it is built(yarn built)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
