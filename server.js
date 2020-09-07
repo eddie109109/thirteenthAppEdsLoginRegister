ac317c9c8c45564ff4070e9aa23730f3666b4564
jundic <jundi_chen@sfu.ca>
2020-09-07 05:42:59 -0700

    added config file

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "build"))); // connect to the static file through express
 
+//direct the user to the built version of react front page, it only works once it is built(yarn built)
+
+// server static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-//direct the user to the built version of react front page, it only works once it is built(yarn built)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
