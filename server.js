ab733b86e1c34c87c354fca4b2ecfd566a96f54d
jundic <jundi_chen@sfu.ca>
2020-09-07 07:26:44 -0700

    fix

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use("/", router);
 
 // server static assets in production
 if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static("client/build"));
+  //   app.use(express.static(path.join(__dirname, "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
