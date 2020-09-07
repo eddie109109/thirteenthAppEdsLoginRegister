1568484be2f40debe6dc4460aa98fd2fe6dba162
jundic <jundi_chen@sfu.ca>
2020-09-07 06:37:17 -0700

    test

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "build"))); // connect to the static file through express
 
 //direct the user to the built version of react front page, it only works once it is built(yarn built)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // server static assets in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
