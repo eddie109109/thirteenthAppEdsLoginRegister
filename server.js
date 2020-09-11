28e81414248d0624824d31d91b117f6fcbf882c7
jundic <jundi_chen@sfu.ca>
2020-09-10 19:56:22 -0700

    make it better

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ app.use(express.static(path.join(__dirname, "client", "build"))); // connect to 
 
 app.use("/", router);
 
-// server static assets in production
+// serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
